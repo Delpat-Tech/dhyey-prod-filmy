959e016bbfdbe7d640278ebdaa7217559d9e0baf
Ashackq <akashpatelyo2@gmail.com>
2025-10-10 10:45:44 +0530

    [update]
    cors and git ignore

--- backend/api.js
+++ backend/api.js
@@ -33,7 +33,7 @@ const app = express();
 // 1) GLOBAL MIDDLEWARES
 // Implement CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://api.dhyey.delpat.in'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
